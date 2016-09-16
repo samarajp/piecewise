2edf2a610757a18701e539b6306b6acbce98fd25
Samara Martins <samara.martins85@gmail.com>
2016-09-16 15:43:40 -0300

    A* com testes para análise

--- piecewise-bus/src/main/java/br/rio/puc/piecewisebus/main/App.java
+++ piecewise-bus/src/main/java/br/rio/puc/piecewisebus/main/App.java
@@ -11,6 +11,7 @@ import br.rio.puc.piecewisebus.function.UpdateStreamFunction;
 import br.rio.puc.piecewisebus.model.Elements;
 import br.rio.puc.piecewisebus.model.Grafo;
 import br.rio.puc.piecewisebus.model.Vertice;
+import br.rio.puc.piecewisebus.shortestpath.AStar;
 
 /**
  * Hello world!
@@ -19,6 +20,7 @@ import br.rio.puc.piecewisebus.model.Vertice;
 public class App {
 	
 	private static ManipulatorR datafunction;
+	private static AStar astar;
 
 	public static void main(String[] args) throws ClassNotFoundException,
 		SQLException, IOException, PiecewiseException {
@@ -34,7 +36,9 @@ public class App {
 		double cost;
 		
 		Grafo grafo = new Grafo();
+		AStar astar = new AStar();
 
+		
 		for (Elements element : elements) {
 
 			Vertice source = grafo.addVertice(element.getSource().trim());
@@ -47,9 +51,12 @@ public class App {
 			datafunction.run(8);
 			
 			cost = datafunction.yFinal;
+			
 			grafo.addAresta(source, target, element.getEdge().trim(), cost);
 			
 			System.out.println(grafo);
+			
+			//astar.shortestpath(source, target);
 			
 		}
 		
@@ -68,5 +75,6 @@ public class App {
 		cost = datafunction.yFinal;
 		
 		System.out.println("Custo da edge que será atualizada: "+cost);
+		
 	}
 }
